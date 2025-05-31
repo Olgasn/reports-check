e8bf1d3f09acda5258ab32944923c482d4b2eb07
vladislavovhich <vladislavovhich@gmail.com>
2025-05-31 23:03:59 +0300

    add install and start files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 5000,
+    open: true,
   },
   preview: {
     port: 5000,
